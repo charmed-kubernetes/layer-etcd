ea61ecddfe4fcd1c7df7ffe3e2a2621dbbd46172
Charles Butler <chuck@dasroot.net>
2016-02-23 01:19:02 -0500

    - changed config option source-path to source-url - refactored etcd.py with some logging

--- lib/etcd.py
+++ lib/etcd.py
@@ -1,19 +1,19 @@
 # This entire file needs formatted and abstracted into a class
 
 # In the older etcd charm there is 2 definite sets of data structures being
-# used.
+# used. This is an attempt to simplify to just:
 #
 # {etcd1: {
 #         'public_address': 127.0.0.1,
 #         'private_address': 127.0.0.1,
-#         'port': 4001,
-#         'unit_name': etcd1,
-#         'management_port': 7001)
+#         'unit_name': etcd1)
 #         }}
 
 from charmhelpers.core.hookenv import unit_get
 from charmhelpers.core.hookenv import is_leader
 from charmhelpers.core.hookenv import leader_get
+from charmhelpers.core.hookenv import config
+
 from charmhelpers.core import unitdata
 from charmhelpers import fetch
 from os import getenv
@@ -26,16 +26,14 @@ from subprocess import check_call
 
 class EtcdHelper:
 
-    def __init__(self, port=4001, management_port=7001):
+    def __init__(self):
         self.db = unitdata.kv()
         self.public_address = unit_get('public-address')
         self.private_address = unit_get('private-address')
         self.hook_data = unitdata.HookData()
         self.unit_name = getenv('JUJU_UNIT_NAME').replace('/', '')
-        if port:
-            self.port = port
-        if management_port:
-            self.management_port = management_port
+        self.port = config('port')
+        self.management_port = config('management_port')
         self.init_cluster_cache()
 
     def init_cluster_cache(self):
@@ -52,6 +50,10 @@ class EtcdHelper:
         return self.db.get('cluster-token')
 
     def id_generator(self, size=6):
+        ''' Return a random 6 character string for use in cluster init.
+
+            @params size - The size of the string to return in characters
+        '''
         chars = string.ascii_uppercase + string.digits
         return ''.join(random.choice(chars) for _ in range(size))
 
@@ -72,12 +74,16 @@ class EtcdHelper:
             target.exists() and target.remove()
             origin.symlink(target)
 
-    def cluster_string(self, cluster='', proto='http'):
+    def cluster_string(self, proto='http'):
         ''' This method behaves slightly different depending on the
             context of its invocation. If the unit is the leader, the
             connection string should always be built and returned from
             the contents in unit data. Otherwise we should return the
-            value set by the leader via leader-data'''
+            value set by the leader via leader-data
+
+            @params proto - Determines the output prefix depending on need. eg:
+                           http://127.0.0.1:4001 or etcd://127.0.0.1:4001
+        '''
         if is_leader():
             cluster_data = self.cluster_data()
             connection_string = ""
@@ -92,7 +98,11 @@ class EtcdHelper:
 
     def cluster_data(self, unit=None):
         ''' Non duplicating merge of dictionaries. Each new node adds a key to
-            the unitdata dict. The dict is the state of the cluster '''
+            the unitdata dict. The dict is the persisted state of the cluster
+            to be used if the leader "goes away". Its the only liferaft in the
+            event of an emergency.
+        '''
+        # ...I'm only mostly certain of this
 
         cluster_data = self.db.get('etcd.cluster_data')
 
@@ -113,28 +123,13 @@ class EtcdHelper:
 
             self.db.set('etcd.cluster_data', cluster_data)
         return cluster_data
-        # Snippet from legacy charm, this greps relation data extracted from
-        # charmhelpers to buid the cluster string. It was honestly black magic
-        # that mostly worked...
-        # if self.hook_data.rels:
-        #     # This feels like highway robbery in terms of encapsulation...
-        #     cluster_rels = self.hook_data.rels['cluster'][1].keys()
-        # introspect the cluster, and form the cluster string.
-        # https://github.com/coreos/etcd/blob/master/Documentation/configuration.md#-initial-cluster
-        #     reldata = self.hook_data.rels['cluster'][1][cluster_rels[0]]
-        #     for unit in reldata:
-        #         private = reldata[unit]['private-address']
-        #         cluster = '{}{}=http://{}:7001,'.format(cluster,
-        #                                            unit.replace('/', ''),
-        #                                              private)
-        # else:
-        #     cluster = "{}=http://{}:{}".format(self.unit_name,
-        #                                        self.private_address,
-        #                                        self.management_port)
-
-        # return cluster.rstrip(',').lstrip(',')
 
     def register(self, cluster_data):
+        ''' Perform self registration against the Etcd leader.
+
+            @params cluster_data - a dict of the data to pass to the
+            etcd leader unit to declare the units intent to join the cluster.
+        '''
         if not self.db.get('registered'):
             command = "etcdctl -C http://{}:{} member add {}" \
                       " http://{}:{}".format(cluster_data['leader_address'],
@@ -145,6 +140,15 @@ class EtcdHelper:
             check_call(split(command))
             self.db.set('registered', True)
 
+    def unregister(self, cluster_data):
+        import pdb; pdb.set_trace()
+        command = "etcdctl -C http://{}:{} member remove {}" \
+                  " http://{}:{}".format(cluster_data['leader_address'],
+                                         config('port'),
+                                         cluster_data['private_address'],
+                                         config('management_port'))
+        check_call(split(command))
+
 
 def databag_to_dict(databag):
     if not hasattr(databag, 'get_remote'):
@@ -153,7 +157,19 @@ def databag_to_dict(databag):
     return {databag.get_remote('unit_name'): {
             'public_address': databag.get_remote('public-address'),
             'private_address': databag.get_remote('private_address'),
-            'port': databag.get_remote('port'),
             'unit_name': databag.get_remote('unit_name'),
-            'management_port': databag.get_remote('management_port')
             }}
+
+
+def remove_unit_from_cache(self, unit_name):
+    ''' Cache data is built with the UNIT_NAME as the key holding
+        the units data. We can expire their cache entry with just
+        the unit name.
+
+        @param - unit_name - the unit name# eg: etcd1
+    '''
+    cluster_data = self.db.get('etcd.cluster_data')
+    cluster_data.pop(unit_name, None)
+    # update with the removed unit
+    self.db.set('etcd.cluster_data', cluster_data)
+    return cluster_data
