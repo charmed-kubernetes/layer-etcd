1b8648d41fa336496efbff47e6a643e550361ee6
Charles Butler <chuck@dasroot.net>
2016-05-09 10:27:13 -0400

    Supporting offline resource files
    These bits add a make target for `fetch_resources`
    
    - Parse the resources.dist file in $CHARM_ROOT
    - Fetch and sha1sum the resources parsed out of resources.dist
    - Print feedback about sum verification, all files isolated in `$CHARM_ROOT/resources`

---
+++ scripts/download_resources.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+#!/bin/bash
+mkdir -p $(pwd)/resources
+
+while IFS="|" read app version file url checksum
+do
+  echo "Downloading $app Version: $version"
+  echo $url
+  echo $file
+  curl -L -v $url -o resources/$file 2>> logfile.txt
+  # make here a special case, if the file is not present
+  calculated_sum=$(sha1sum "resources/$file" | /usr/bin/cut -f 1 -d " ")
+  # compare checksum
+  case "$calculated_sum" in
+    "$checksum")
+      echo -e " \033[m\033[42m  OK  \033[0m crypto signature compared and correct.";
+	;;
+	*)
+	echo -e " \033[m\033[41m  ERROR  \033[0m cryptographic verification failed!";
+  esac
+done < "resources.dist"
