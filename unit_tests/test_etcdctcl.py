ebea998a68731a7516591a2f8e5e718edd392418
Charles Butler <chuck@dasroot.net>
2016-06-07 14:22:51 -0400

    lint fixes
    as described

--- unit_tests/test_etcdctcl.py
+++ unit_tests/test_etcdctcl.py
@@ -15,7 +15,7 @@ class TestEtcdCtl:
             self.etcdctl().register({'private_address': '127.0.0.1',
                                      'unit_name': 'etcd0',
                                      'management_port': '1313',
-                                     'leader_address': 'http://127.1.1.1:1212'})
+                                     'leader_address': 'http://127.1.1.1:1212'})  # noqa
             spcm.assert_called_with(['etcdctl',
                                      '-C',
                                      'http://127.1.1.1:1212',
@@ -43,5 +43,5 @@ class TestEtcdCtl:
 
     def test_member_list_with_unstarted_member(self):
         # 57fa5c39949c138e[unstarted]: peerURLs=http://10.113.96.80:2380
-        # bb0f83ebb26386f7: name=etcd9 peerURLs=https://10.113.96.178:2380 clientURLs=https://10.113.96.178:2379
+        # bb0f83ebb26386f7: name=etcd9 peerURLs=https://10.113.96.178:2380 clientURLs=https://10.113.96.178:2379  # noqa
         pass
