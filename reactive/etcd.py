3d9f9ed59b98dbb91c530bcf4a3280a83f91e289
axino <axino@canonical.com>
2017-01-18 18:53:57 +0100

    add and nrpe-external-master relation, with a basic check (systemd service is running)

--- reactive/etcd.py
+++ reactive/etcd.py
@@ -25,6 +25,7 @@ from charmhelpers.core import host
 from charmhelpers.core import unitdata
 from charmhelpers.fetch import apt_update
 from charmhelpers.fetch import apt_install
+from charmhelpers.contrib.charmsupport import nrpe
 
 from etcdctl import EtcdCtl
 from etcdctl import get_connection_string
@@ -487,6 +488,30 @@ def format_and_mount_storage():
 
     # Finally re-establish etcd operation
     host.service_restart('etcd')
+
+
+@when('nrpe-external-master.available')
+@when_not('nrpe-external-master.initial-config')
+def initial_nrpe_config(nagios=None):
+    set_state('nrpe-external-master.initial-config')
+    update_nrpe_config(nagios)
+
+
+@when('etcd.installed')
+@when('nrpe-external-master.available')
+@when_any('config.changed.nagios_context',
+          'config.changed.nagios_servicegroups')
+def update_nrpe_config(unused=None):
+    # List of systemd services that will be checked
+    services = ('etcd',)
+
+    # The current nrpe-external-master interface doesn't handle a lot of logic,
+    # use the charm-helpers code for now.
+    hostname = nrpe.get_nagios_hostname()
+    current_unit = nrpe.get_nagios_unit_name()
+    nrpe_setup = nrpe.NRPE(hostname=hostname, primary=False)
+    nrpe.add_init_service_checks(nrpe_setup, services, current_unit)
+    nrpe_setup.write()
 
 
 def volume_is_mounted(volume):
