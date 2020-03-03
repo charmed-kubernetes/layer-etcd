3ac70e3f881cb19a4fff399fc0c33777a824f31b
Barry Price <barryprice@users.noreply.github.com>
2020-03-03 21:01:49 +0700

    Alarms (#168)
    * Add an etcd alarms nagios check LP#1828170

---
+++ templates/check_etcd-alarms.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2020 Canonical Ltd.
+
+import nagios_plugin3
+
+
+def load_alarm_list():
+    """Load the cached status from disk, return it as a string"""
+    alarm_list_path = '/var/lib/nagios/etcd-alarm-list.txt'
+
+    with open(alarm_list_path, 'r') as alarm_list_log:
+        alarm_list = alarm_list_log.read()
+
+    return alarm_list.strip()
+
+
+def check_alarms():
+    """Raise an error if the cached status contains any non-blank lines"""
+    alarms = []
+    alarm_list = load_alarm_list()
+    for line in alarm_list.splitlines():
+        line = line.strip()
+        if line:
+            alarms.append(line)
+    if alarms:
+        raise nagios_plugin3.CriticalError(' '.join(alarms))
+
+
+def main():
+    nagios_plugin3.try_check(check_alarms)
+    print("OK - no active alarms")
+
+
+if __name__ == "__main__":
+    main()
