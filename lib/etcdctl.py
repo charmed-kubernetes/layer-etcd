a3e1e8aed51120ad0e4d9ae3fb4e05dfd9953cdd
Adam Dyess <adam.dyess@canonical.com>
2022-01-27 15:38:12 -0600

    Blacken project

--- lib/etcdctl.py
+++ lib/etcdctl.py
@@ -6,40 +6,39 @@ import os
 
 
 def etcdctl_command():
-    if os.path.isfile('/snap/bin/etcd.etcdctl'):
-        return '/snap/bin/etcd.etcdctl'
-    return 'etcdctl'
+    if os.path.isfile("/snap/bin/etcd.etcdctl"):
+        return "/snap/bin/etcd.etcdctl"
+    return "etcdctl"
 
 
 class EtcdCtl:
-    ''' etcdctl modeled as a python class. This python wrapper consumes
+    """etcdctl modeled as a python class. This python wrapper consumes
     and exposes some of the commands contained in etcdctl. Related to unit
-    registration, cluster health, and other operations '''
+    registration, cluster health, and other operations"""
+
     class CommandFailed(Exception):
         pass
 
     def register(self, cluster_data):
-        ''' Perform self registration against the etcd leader and returns the
+        """Perform self registration against the etcd leader and returns the
         raw output response.
 
         @params cluster_data - a dict of data to fill out the request to
         push our registration to the leader
         requires keys: leader_address, port, unit_name, cluster_address,
         management_port
-        '''
+        """
         # Build a connection string for the cluster data.
-        connection = get_connection_string([cluster_data['cluster_address']],
-                                           cluster_data['management_port'])
-
-        command = 'member add {} {}'.format(
-            cluster_data['unit_name'],
-            connection
+        connection = get_connection_string(
+            [cluster_data["cluster_address"]], cluster_data["management_port"]
         )
 
+        command = "member add {} {}".format(cluster_data["unit_name"], connection)
+
         try:
-            result = self.run(command, endpoints=cluster_data['leader_address'], api=2)
+            result = self.run(command, endpoints=cluster_data["leader_address"], api=2)
         except EtcdCtl.CommandFailed:
-            log('Notice:  Unit failed self registration', 'WARNING')
+            log("Notice:  Unit failed self registration", "WARNING")
             raise
 
         # ['Added member named etcd12 with ID b9ab5b5a2e4baec5 to cluster',
@@ -49,117 +48,118 @@ class EtcdCtl:
 
         reg = {}
 
-        for line in result.split('\n'):
-            if 'ETCD_INITIAL_CLUSTER=' in line:
-                reg['cluster'] = line.split('="')[-1].rstrip('"')
+        for line in result.split("\n"):
+            if "ETCD_INITIAL_CLUSTER=" in line:
+                reg["cluster"] = line.split('="')[-1].rstrip('"')
         return reg
 
     def unregister(self, unit_id, leader_address=None):
-        ''' Perform self deregistration during unit teardown
+        """Perform self deregistration during unit teardown
 
         @params unit_id - the ID for the unit assigned by etcd. Obtainable from
         member_list method.
 
         @params leader_address - The endpoint to communicate with the leader in
         the event of self deregistration.
-        '''
-        return self.run(['member', 'remove', unit_id], endpoints=leader_address, api=2)
+        """
+        return self.run(["member", "remove", unit_id], endpoints=leader_address, api=2)
 
     def member_list(self, leader_address=False):
-        ''' Returns the output from `etcdctl member list` as a python dict
+        """Returns the output from `etcdctl member list` as a python dict
         organized by unit_name, containing all the data-points in the resulting
-        response. '''
-        command = 'member list'
+        response."""
+        command = "member list"
 
         members = {}
         out = self.run(command, endpoints=leader_address, api=2)
-        raw_member_list = out.strip('\n').split('\n')
+        raw_member_list = out.strip("\n").split("\n")
         # Expect output like this:
         # 4f24ee16c889f6c1: name=etcd20 peerURLs=https://10.113.96.197:2380 clientURLs=https://10.113.96.197:2379  # noqa
         # edc04bb81479d7e8: name=etcd21 peerURLs=https://10.113.96.243:2380 clientURLs=https://10.113.96.243:2379  # noqa
         # edc0dsa81479d7e8[unstarted]: peerURLs=https://10.113.96.124:2380  # noqa
 
         for unit in raw_member_list:
-            if '[unstarted]' in unit:
-                unit_guid = unit.split('[')[0]
-                members['unstarted'] = {'unit_id': unit_guid}
-                if 'peerURLs=' in unit:
-                    peer_urls = unit.split(' ')[1].split("=")[-1]
-                    members['unstarted']['peer_urls'] = peer_urls
+            if "[unstarted]" in unit:
+                unit_guid = unit.split("[")[0]
+                members["unstarted"] = {"unit_id": unit_guid}
+                if "peerURLs=" in unit:
+                    peer_urls = unit.split(" ")[1].split("=")[-1]
+                    members["unstarted"]["peer_urls"] = peer_urls
                 continue
-            unit_guid = unit.split(':')[0]
-            unit_name = unit.split(' ')[1].split("=")[-1]
-            peer_urls = unit.split(' ')[2].split("=")[-1]
-            client_urls = unit.split(' ')[3].split("=")[-1]
+            unit_guid = unit.split(":")[0]
+            unit_name = unit.split(" ")[1].split("=")[-1]
+            peer_urls = unit.split(" ")[2].split("=")[-1]
+            client_urls = unit.split(" ")[3].split("=")[-1]
 
-            members[unit_name] = {'unit_id': unit_guid,
-                                  'name': unit_name,
-                                  'peer_urls': peer_urls,
-                                  'client_urls': client_urls}
+            members[unit_name] = {
+                "unit_id": unit_guid,
+                "name": unit_name,
+                "peer_urls": peer_urls,
+                "client_urls": client_urls,
+            }
         return members
 
     def member_update(self, unit_id, uri):
-        ''' Update the etcd cluster member by unit_id with a new uri. This
+        """Update the etcd cluster member by unit_id with a new uri. This
         allows us to change protocol, address or port.
         @params unit_id: The string ID of the unit in the cluster.
         @params uri: The string universal resource indicator of where to
-        contact the peer. '''
-        out = ''
+        contact the peer."""
+        out = ""
         try:
-            command = 'member update {} {}'.format(unit_id, uri)
+            command = "member update {} {}".format(unit_id, uri)
             log(command)
             # Run the member update command for the existing unit_id.
             out = self.run(command)
         except EtcdCtl.CommandFailed:
-            log('Failed to update member {}'.format(unit_id), 'WARNING')
+            log("Failed to update member {}".format(unit_id), "WARNING")
         return out
 
     def cluster_health(self, output_only=False):
-        ''' Returns the output of etcdctl cluster-health as a python dict
-        organized by topical information with detailed unit output '''
+        """Returns the output of etcdctl cluster-health as a python dict
+        organized by topical information with detailed unit output"""
         health = {}
         try:
-            out = self.run('cluster-health', endpoints=False, api=2)
+            out = self.run("cluster-health", endpoints=False, api=2)
             if output_only:
                 return out
-            health_output = out.strip('\n').split('\n')
-            health['status'] = health_output[-1]
-            health['units'] = health_output[0:-2]
+            health_output = out.strip("\n").split("\n")
+            health["status"] = health_output[-1]
+            health["units"] = health_output[0:-2]
         except EtcdCtl.CommandFailed:
-            log('Notice:  Unit failed cluster-health check', 'WARNING')
-            health['status'] = 'cluster is unhealthy see log file for details.'
-            health['units'] = []
+            log("Notice:  Unit failed cluster-health check", "WARNING")
+            health["status"] = "cluster is unhealthy see log file for details."
+            health["units"] = []
         return health
 
     def run(self, arguments, endpoints=None, api=3):
-        ''' Wrapper to subprocess calling output. This is a convenience
-        method to clean up the calls to subprocess and append TLS data'''
+        """Wrapper to subprocess calling output. This is a convenience
+        method to clean up the calls to subprocess and append TLS data"""
         env = {}
         command = [etcdctl_command()]
-        opts = layer.options('tls-client')
-        ca_path = opts['ca_certificate_path']
-        crt_path = opts['server_certificate_path']
-        key_path = opts['server_key_path']
+        opts = layer.options("tls-client")
+        ca_path = opts["ca_certificate_path"]
+        crt_path = opts["server_certificate_path"]
+        key_path = opts["server_key_path"]
 
         if api == 3:
-            env['ETCDCTL_API'] = '3'
-            env['ETCDCTL_CACERT'] = ca_path
-            env['ETCDCTL_CERT'] = crt_path
-            env['ETCDCTL_KEY'] = key_path
+            env["ETCDCTL_API"] = "3"
+            env["ETCDCTL_CACERT"] = ca_path
+            env["ETCDCTL_CERT"] = crt_path
+            env["ETCDCTL_KEY"] = key_path
             if endpoints is None:
-                endpoints = 'http://127.0.0.1:4001'
+                endpoints = "http://127.0.0.1:4001"
 
         elif api == 2:
-            env['ETCDCTL_API'] = '2'
-            env['ETCDCTL_CA_FILE'] = ca_path
-            env['ETCDCTL_CERT_FILE'] = crt_path
-            env['ETCDCTL_KEY_FILE'] = key_path
+            env["ETCDCTL_API"] = "2"
+            env["ETCDCTL_CA_FILE"] = ca_path
+            env["ETCDCTL_CERT_FILE"] = crt_path
+            env["ETCDCTL_KEY_FILE"] = key_path
             if endpoints is None:
-                endpoints = ':4001'
+                endpoints = ":4001"
 
         else:
-            raise NotImplementedError(
-                'etcd api version {} not supported'.format(api))
+            raise NotImplementedError("etcd api version {} not supported".format(api))
 
         if isinstance(arguments, str):
             command.extend(arguments.split())
@@ -167,48 +167,43 @@ class EtcdCtl:
             command.extend(arguments)
         else:
             raise RuntimeError(
-                'arguments not correct type; must be string, list or tuple')
+                "arguments not correct type; must be string, list or tuple"
+            )
 
         if endpoints is not False:
             if api == 3:
-                command.extend(['--endpoints', endpoints])
+                command.extend(["--endpoints", endpoints])
             elif api == 2:
-                command.insert(1, '--endpoint')
+                command.insert(1, "--endpoint")
                 command.insert(2, endpoints)
 
         try:
-            return check_output(
-                command,
-                env=env
-            ).decode('utf-8')
+            return check_output(command, env=env).decode("utf-8")
         except CalledProcessError as e:
-            log(command, 'ERROR')
-            log(env, 'ERROR')
-            log(e.stdout, 'ERROR')
-            log(e.stderr, 'ERROR')
+            log(command, "ERROR")
+            log(env, "ERROR")
+            log(e.stdout, "ERROR")
+            log(e.stderr, "ERROR")
             raise EtcdCtl.CommandFailed() from e
 
     def version(self):
-        ''' Return the version of etcdctl '''
+        """Return the version of etcdctl"""
         out = check_output(
-            [etcdctl_command(), 'version'],
-            env={'ETCDCTL_API': '3'}
-        ).decode('utf-8')
+            [etcdctl_command(), "version"], env={"ETCDCTL_API": "3"}
+        ).decode("utf-8")
 
         if out == "No help topic for 'version'\n":
             # Probably on etcd2
-            out = check_output(
-                [etcdctl_command(), '--version']
-            ).decode('utf-8')
+            out = check_output([etcdctl_command(), "--version"]).decode("utf-8")
 
-        return out.split('\n')[0].split()[2]
+        return out.split("\n")[0].split()[2]
 
 
-def get_connection_string(members, port, protocol='https'):
-    ''' Return a connection string for the list of members using the provided
-    port and protocol (defaults to https)'''
+def get_connection_string(members, port, protocol="https"):
+    """Return a connection string for the list of members using the provided
+    port and protocol (defaults to https)"""
     connections = []
     for address in members:
-        connections.append('{}://{}:{}'.format(protocol, address, port))
-    connection_string = ','.join(connections)
+        connections.append("{}://{}:{}".format(protocol, address, port))
+    connection_string = ",".join(connections)
     return connection_string
--- tests/20-actions.py
+++ tests/20-actions.py
@@ -11,28 +11,28 @@ import amulet
 class TestActions(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.d = amulet.Deployment(series='xenial')
-        cls.d.add('etcd')
-        cls.d.add('easyrsa', 'cs:~containers/easyrsa')
-        cls.d.configure('etcd', {'channel': '3.0/stable'})
-        cls.d.relate('easyrsa:client', 'etcd:certificates')
+        cls.d = amulet.Deployment(series="xenial")
+        cls.d.add("etcd")
+        cls.d.add("easyrsa", "cs:~containers/easyrsa")
+        cls.d.configure("etcd", {"channel": "3.0/stable"})
+        cls.d.relate("easyrsa:client", "etcd:certificates")
         cls.d.setup(timeout=1200)
-        cls.d.sentry.wait_for_messages({'etcd':
-                                        re.compile('Healthy*|Unhealthy*')})
+        cls.d.sentry.wait_for_messages({"etcd": re.compile("Healthy*|Unhealthy*")})
         # cls.d.sentry.wait()
-        cls.etcd = cls.d.sentry['etcd']
+        cls.etcd = cls.d.sentry["etcd"]
 
     def test_health_check(self):
         """
         Trigger health action
         """
-        action_id = self.etcd[0].run_action('health')
-        outcome = self.d.action_fetch(action_id,
-                                      timeout=7200,
-                                      raise_on_timeout=True,
-                                      full_output=True)
-        self.assertEqual(outcome['status'], 'completed')
-        self.assertTrue("cluster is healthy" in outcome['results']['result-map']['message'])
+        action_id = self.etcd[0].run_action("health")
+        outcome = self.d.action_fetch(
+            action_id, timeout=7200, raise_on_timeout=True, full_output=True
+        )
+        self.assertEqual(outcome["status"], "completed")
+        self.assertTrue(
+            "cluster is healthy" in outcome["results"]["result-map"]["message"]
+        )
 
     def test_snapshot_restore(self):
         """
@@ -40,113 +40,126 @@ class TestActions(unittest.TestCase):
         """
         # Load dummy data
         self.load_data()
-        self.assertTrue(self.is_data_present('v2'))
-        self.assertTrue(self.is_data_present('v3'))
+        self.assertTrue(self.is_data_present("v2"))
+        self.assertTrue(self.is_data_present("v3"))
 
         filenames = {}
-        for dataset in ['v2', 'v3']:
+        for dataset in ["v2", "v3"]:
             # Take snapshot of data
-            action_id = self.etcd[0].run_action('snapshot', {'keys-version': dataset})
-            outcome = self.d.action_fetch(action_id,
-                                          timeout=7200,
-                                          raise_on_timeout=True,
-                                          full_output=True)
-            self.assertEqual(outcome['status'], 'completed')
-            cpcmd = outcome['results']['copy']['cmd']
+            action_id = self.etcd[0].run_action("snapshot", {"keys-version": dataset})
+            outcome = self.d.action_fetch(
+                action_id, timeout=7200, raise_on_timeout=True, full_output=True
+            )
+            self.assertEqual(outcome["status"], "completed")
+            cpcmd = outcome["results"]["copy"]["cmd"]
             subprocess.check_call(cpcmd.split())
-            filenames[dataset] = os.path.basename(outcome['results']['snapshot']['path'])
+            filenames[dataset] = os.path.basename(
+                outcome["results"]["snapshot"]["path"]
+            )
 
         self.delete_data()
-        self.assertFalse(self.is_data_present('v2'))
-        self.assertFalse(self.is_data_present('v3'))
+        self.assertFalse(self.is_data_present("v2"))
+        self.assertFalse(self.is_data_present("v3"))
 
         # Restore v2 data
-        cmd = 'juju attach etcd snapshot=%s' % filenames['v2']
+        cmd = "juju attach etcd snapshot=%s" % filenames["v2"]
         subprocess.check_call(cmd.split())
-        action_id = self.etcd[0].run_action('restore')
-        outcome = self.d.action_fetch(action_id,
-                                      timeout=7200,
-                                      raise_on_timeout=True,
-                                      full_output=True)
-        self.assertEqual(outcome['status'], 'completed')
-        self.assertTrue(self.is_data_present('v2'))
-        self.assertFalse(self.is_data_present('v3'))
+        action_id = self.etcd[0].run_action("restore")
+        outcome = self.d.action_fetch(
+            action_id, timeout=7200, raise_on_timeout=True, full_output=True
+        )
+        self.assertEqual(outcome["status"], "completed")
+        self.assertTrue(self.is_data_present("v2"))
+        self.assertFalse(self.is_data_present("v3"))
 
         # Restore v3 data
-        cmd = 'juju attach etcd snapshot=%s' % filenames['v3']
+        cmd = "juju attach etcd snapshot=%s" % filenames["v3"]
         subprocess.check_call(cmd.split())
-        action_id = self.etcd[0].run_action('restore')
-        outcome = self.d.action_fetch(action_id,
-                                      timeout=7200,
-                                      raise_on_timeout=True,
-                                      full_output=True)
-        self.assertEqual(outcome['status'], 'completed')
-        self.assertFalse(self.is_data_present('v2'))
-        self.assertTrue(self.is_data_present('v3'))
+        action_id = self.etcd[0].run_action("restore")
+        outcome = self.d.action_fetch(
+            action_id, timeout=7200, raise_on_timeout=True, full_output=True
+        )
+        self.assertEqual(outcome["status"], "completed")
+        self.assertFalse(self.is_data_present("v2"))
+        self.assertTrue(self.is_data_present("v3"))
 
     def load_data(self):
         """
         Load dummy data
 
         """
-        certs = "ETCDCTL_KEY_FILE=/var/snap/etcd/common/client.key " \
-                "ETCDCTL_CERT_FILE=/var/snap/etcd/common/client.crt " \
-                "ETCDCTL_CA_FILE=/var/snap/etcd/common/ca.crt " \
-                "ETCDCTL_KEY=/var/snap/etcd/common/client.key " \
-                "ETCDCTL_CERT=/var/snap/etcd/common/client.crt " \
-                "ETCDCTL_CACERT=/var/snap/etcd/common/ca.crt"
+        certs = (
+            "ETCDCTL_KEY_FILE=/var/snap/etcd/common/client.key "
+            "ETCDCTL_CERT_FILE=/var/snap/etcd/common/client.crt "
+            "ETCDCTL_CA_FILE=/var/snap/etcd/common/ca.crt "
+            "ETCDCTL_KEY=/var/snap/etcd/common/client.key "
+            "ETCDCTL_CERT=/var/snap/etcd/common/client.crt "
+            "ETCDCTL_CACERT=/var/snap/etcd/common/ca.crt"
+        )
 
-        cmd = '{} ETCDCTL_API=2 /snap/bin/etcdctl set /etcd2key etcd2value'.format(certs)
+        cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl set /etcd2key etcd2value".format(
+            certs
+        )
         self.etcd[0].run(cmd)
-        cmd = '{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 ' \
-              'put etcd3key etcd3value'.format(certs)
+        cmd = (
+            "{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 "
+            "put etcd3key etcd3value".format(certs)
+        )
         self.etcd[0].run(cmd)
 
     def is_data_present(self, version):
-        '''
+        """
         Check if we have the data present on the datastore of the version
         Args:
             version: v2 or v3 etcd datastore
 
         Returns: True if the data is present
 
-        '''
-        certs = "ETCDCTL_KEY_FILE=/var/snap/etcd/common/client.key " \
-                "ETCDCTL_CERT_FILE=/var/snap/etcd/common/client.crt " \
-                "ETCDCTL_CA_FILE=/var/snap/etcd/common/ca.crt " \
-                "ETCDCTL_KEY=/var/snap/etcd/common/client.key " \
-                "ETCDCTL_CERT=/var/snap/etcd/common/client.crt " \
-                "ETCDCTL_CACERT=/var/snap/etcd/common/ca.crt"
+        """
+        certs = (
+            "ETCDCTL_KEY_FILE=/var/snap/etcd/common/client.key "
+            "ETCDCTL_CERT_FILE=/var/snap/etcd/common/client.crt "
+            "ETCDCTL_CA_FILE=/var/snap/etcd/common/ca.crt "
+            "ETCDCTL_KEY=/var/snap/etcd/common/client.key "
+            "ETCDCTL_CERT=/var/snap/etcd/common/client.crt "
+            "ETCDCTL_CACERT=/var/snap/etcd/common/ca.crt"
+        )
 
-        if version == 'v2':
-            cmd = '{} ETCDCTL_API=2 /snap/bin/etcdctl ls'.format(certs)
+        if version == "v2":
+            cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl ls".format(certs)
             data = self.etcd[0].run(cmd)
-            return 'etcd2key' in data[0]
-        elif version == 'v3':
-            cmd = '{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 ' \
-                  'get "" --prefix --keys-only'.format(certs)
+            return "etcd2key" in data[0]
+        elif version == "v3":
+            cmd = (
+                "{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 "
+                'get "" --prefix --keys-only'.format(certs)
+            )
             data = self.etcd[0].run(cmd)
-            return 'etcd3key' in data[0]
+            return "etcd3key" in data[0]
         else:
             return False
 
     def delete_data(self):
-        '''
+        """
         Delete all dummy data on etcd
-        '''
-        certs = "ETCDCTL_KEY_FILE=/var/snap/etcd/common/client.key " \
-                "ETCDCTL_CERT_FILE=/var/snap/etcd/common/client.crt " \
-                "ETCDCTL_CA_FILE=/var/snap/etcd/common/ca.crt " \
-                "ETCDCTL_KEY=/var/snap/etcd/common/client.key " \
-                "ETCDCTL_CERT=/var/snap/etcd/common/client.crt " \
-                "ETCDCTL_CACERT=/var/snap/etcd/common/ca.crt"
+        """
+        certs = (
+            "ETCDCTL_KEY_FILE=/var/snap/etcd/common/client.key "
+            "ETCDCTL_CERT_FILE=/var/snap/etcd/common/client.crt "
+            "ETCDCTL_CA_FILE=/var/snap/etcd/common/ca.crt "
+            "ETCDCTL_KEY=/var/snap/etcd/common/client.key "
+            "ETCDCTL_CERT=/var/snap/etcd/common/client.crt "
+            "ETCDCTL_CACERT=/var/snap/etcd/common/ca.crt"
+        )
 
-        cmd = '{} ETCDCTL_API=2 /snap/bin/etcdctl rm /etcd2key'.format(certs)
+        cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl rm /etcd2key".format(certs)
         self.etcd[0].run(cmd)
-        cmd = '{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 ' \
-              'del etcd3key'.format(certs)
+        cmd = (
+            "{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 "
+            "del etcd3key".format(certs)
+        )
         self.etcd[0].run(cmd)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
