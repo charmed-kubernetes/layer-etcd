24719d817bbfad9571df0e8240623491ae7bcf91
Adam Dyess <adam.dyess@canonical.com>
2022-07-22 08:46:53 -0500

    Jammy support (#200)
    * enable jammy support for this charm
    
    * blacken actions/ sources
    
    * unpin jinja requirements to use native charmhelpers tools
    
    * Integration testing by series, including jammy
    
    * for etcd v2 snapshot, dump to /var/snap/etcd/... then copy to HOME afterwards
    
    * Update metadata.yaml
    
    * update snapshot action to move from `/var/snap/etcd/...` to the backup target dir
    
    * remove wheelhouse.txt since pinning is no longer necessary
    
    * remove xenial

--- actions/restore.py
+++ actions/restore.py
@@ -1,9 +1,7 @@
 #!/usr/local/sbin/charm-env python3
 
-import random
 from charms import layer
-from charms.templating.jinja2 import render
-from charmhelpers.core import unitdata
+from charmhelpers.core.templating import render
 from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import function_fail
 from charmhelpers.core.hookenv import action_get
@@ -38,60 +36,59 @@ import yaml
 layer.import_layer_libs()
 _run_atstart()
 
-opts = layer.options('etcd')
+opts = layer.options("etcd")
 
-DATESTAMP = datetime.strftime(datetime.now(), '%Y%m%d-%H%M%S')
+DATESTAMP = datetime.strftime(datetime.now(), "%Y%m%d-%H%M%S")
 ARCHIVE = "etcd-data-{}.tar.gz".format(DATESTAMP)
 
-unit_name = os.getenv('JUJU_UNIT_NAME').replace('/', '')
-ETCD_DATA_DIR = '{}/{}.etcd'.format(opts['etcd_data_dir'], unit_name)
+unit_name = os.getenv("JUJU_UNIT_NAME").replace("/", "")
+ETCD_DATA_DIR = "{}/{}.etcd".format(opts["etcd_data_dir"], unit_name)
 if not os.path.isdir(ETCD_DATA_DIR):
-    ETCD_DATA_DIR = opts['etcd_data_dir']
+    ETCD_DATA_DIR = opts["etcd_data_dir"]
 
-ETCD_PORT = config('management_port')
-CLUSTER_ADDRESS = get_ingress_address('cluster')
-SKIP_BACKUP = action_get('skip-backup')
-SNAPSHOT_ARCHIVE = resource_get('snapshot')
-TARGET_PATH = action_get('target')
+ETCD_PORT = config("management_port")
+CLUSTER_ADDRESS = get_ingress_address("cluster")
+SKIP_BACKUP = action_get("skip-backup")
+SNAPSHOT_ARCHIVE = resource_get("snapshot")
+TARGET_PATH = action_get("target")
 
 
 def preflight_check():
-    ''' Check preconditions for data restoration '''
+    """Check preconditions for data restoration"""
     if not is_leader():
-        function_fail('This action can only be run on the leader unit')
+        function_fail("This action can only be run on the leader unit")
         sys.exit(0)
     if not SNAPSHOT_ARCHIVE:
-        function_fail({'result.failed': 'Missing snapshot. See: README.md'})
+        function_fail({"result.failed": "Missing snapshot. See: README.md"})
         sys.exit(0)
 
 
 def render_backup():
-    ''' Backup existing data in the event of restoration on a dirty unit. '''
+    """Backup existing data in the event of restoration on a dirty unit."""
     if not os.path.isdir(ETCD_DATA_DIR) and SKIP_BACKUP:
         msg = "Backup set to True, but no data found to backup"
-        action_set({'backup.error': msg})
+        action_set({"backup.error": msg})
     if not os.path.isdir(ETCD_DATA_DIR):
         return
 
     with chdir(ETCD_DATA_DIR):
         if not SKIP_BACKUP:
-            log('Backing up existing data found in {}'.format(ETCD_DATA_DIR))
+            log("Backing up existing data found in {}".format(ETCD_DATA_DIR))
             archive_path = "{}/{}".format(TARGET_PATH, ARCHIVE)
-            cmd = 'tar cvf {0} {1}'.format(archive_path, '.')
+            cmd = "tar cvf {0} {1}".format(archive_path, ".")
             check_call(split(cmd))
             backup_sum = shasum_file(archive_path)
-            action_set({'backup.path': archive_path,
-                        'backup.sha256sum': backup_sum})
+            action_set({"backup.path": archive_path, "backup.sha256sum": backup_sum})
 
 
 def unpack_resource():
-    ''' Grab the resource path, and unpack it into $PATH '''
+    """Grab the resource path, and unpack it into $PATH"""
     cmd = "tar xvf {0} -C {1}".format(SNAPSHOT_ARCHIVE, ETCD_DATA_DIR)
     check_call(split(cmd))
 
 
 def is_v3_backup():
-    ''' See if the backup file does not contain a wal file indicating a v3 backup '''
+    """See if the backup file does not contain a wal file indicating a v3 backup"""
     # ETCD v3 doesn't contain a wal file which leads to below command to fail
     # With that we can differentiate between v3 and v2&v1
     cmd = "tar -tvf {0} --wildcards '*/wal'".format(SNAPSHOT_ARCHIVE)
@@ -103,48 +100,51 @@ def is_v3_backup():
 
 
 def restore_v3_backup():
-    ''' Apply a v3 backup '''
+    """Apply a v3 backup"""
     cmd = "mkdir -p /root/tmp/restore-v3"
     check_call(split(cmd))
 
     cmd = "tar xvf {0} -C /root/tmp/restore-v3".format(SNAPSHOT_ARCHIVE)
     check_call(split(cmd))
 
-    configfile = open('/var/snap/etcd/common/etcd.conf.yml', "r")
+    configfile = open("/var/snap/etcd/common/etcd.conf.yml", "r")
     config = yaml.safe_load(configfile)
     # Use the insecure 4001 port we have open in our deployment
     environ = dict(os.environ, ETCDCTL_API="3")
-    cmd = "/snap/bin/etcdctl --endpoints=http://localhost:4001 snapshot " \
-          "restore /root/tmp/restore-v3/db --skip-hash-check " \
-          "--data-dir='/root/tmp/restore-v3/etcd' " \
-          "--initial-cluster='{}' --initial-cluster-token='{}' " \
-          "--initial-advertise-peer-urls='{}' --name='{}'"
+    cmd = (
+        "/snap/bin/etcdctl --endpoints=http://localhost:4001 snapshot "
+        "restore /root/tmp/restore-v3/db --skip-hash-check "
+        "--data-dir='/root/tmp/restore-v3/etcd' "
+        "--initial-cluster='{}' --initial-cluster-token='{}' "
+        "--initial-advertise-peer-urls='{}' --name='{}'"
+    )
 
-    if 'initial-cluster' in config and config['initial-cluster']:
+    if "initial-cluster" in config and config["initial-cluster"]:
         # configuration contains initilization params
-        cmd = cmd.format(config['initial-cluster'],
-                         config['initial-cluster-token'],
-                         config['initial-advertise-peer-urls'],
-                         config['name'])
+        cmd = cmd.format(
+            config["initial-cluster"],
+            config["initial-cluster-token"],
+            config["initial-advertise-peer-urls"],
+            config["name"],
+        )
     else:
         # configuration does not contain initilization params
         # probably coming from an etcd upgrades from etcd2
-        initial_cluster = '{}=https://{}:2380'.format(config['name'], CLUSTER_ADDRESS)
+        initial_cluster = "{}=https://{}:2380".format(config["name"], CLUSTER_ADDRESS)
         initial_cluster_token = CLUSTER_ADDRESS
-        initial_urls = 'https://{}:2380'.format(CLUSTER_ADDRESS)
-        cmd = cmd.format(initial_cluster,
-                         initial_cluster_token,
-                         initial_urls,
-                         config['name'])
+        initial_urls = "https://{}:2380".format(CLUSTER_ADDRESS)
+        cmd = cmd.format(
+            initial_cluster, initial_cluster_token, initial_urls, config["name"]
+        )
 
     configfile.close()
     check_call(split(cmd), env=environ)
 
     # Make sure we do not have anything left from any old deployments
-    cmd = "rm -rf {}/member".format(config['data-dir'])
+    cmd = "rm -rf {}/member".format(config["data-dir"])
     check_call(split(cmd))
 
-    cmd = "cp -r /root/tmp/restore-v3/etcd/member {}".format(config['data-dir'])
+    cmd = "cp -r /root/tmp/restore-v3/etcd/member {}".format(config["data-dir"])
     check_call(split(cmd))
 
     # Clean up
@@ -153,36 +153,36 @@ def restore_v3_backup():
 
 
 def start_etcd_forked():
-    ''' Start the etcd daemon temporarily to initiate new cluster details '''
+    """Start the etcd daemon temporarily to initiate new cluster details"""
     raw = "/snap/etcd/current/bin/etcd -data-dir={0} -force-new-cluster --enable-v2"
     cmd = raw.format(ETCD_DATA_DIR)
     proc = Popen(split(cmd), stdout=PIPE, stderr=PIPE)
     return proc.pid
 
 
-def pkill_etcd(pid=''):
-    ''' Kill the temporary forked etcd daemon '''
+def pkill_etcd(pid=""):
+    """Kill the temporary forked etcd daemon"""
     # cmd = 'pkill etcd'
     if pid:
-        cmd = 'kill -9 {}'.format(pid)
+        cmd = "kill -9 {}".format(pid)
     else:
-        cmd = 'pkill etcd'
+        cmd = "pkill etcd"
 
     check_call(split(cmd))
 
 
 def probe_forked_etcd():
-    ''' Block until the forked etcd instance has started and return'''
+    """Block until the forked etcd instance has started and return"""
     output = b""
     loop = 0
     MAX_WAIT = 10
 
     while b"http://localhost" not in output:
         try:
-            output = check_output(split('/snap/bin/etcd.etcdctl member list'))
+            output = check_output(split("/snap/bin/etcd.etcdctl member list"))
             loop = loop + 1
         except:
-            log('Still waiting on forked etcd instance...')
+            log("Still waiting on forked etcd instance...")
             output = b""
             loop = loop + 1
         time.sleep(1)
@@ -191,8 +191,8 @@ def probe_forked_etcd():
 
 
 def reconfigure_client_advertise():
-    ''' Reconfigure the backup to use host network addresses for client advertise
-        instead of the assumed localhost addressing '''
+    """Reconfigure the backup to use host network addresses for client advertise
+    instead of the assumed localhost addressing"""
 
     loop = 0
     MAX_WAIT = 10
@@ -201,7 +201,7 @@ def reconfigure_client_advertise():
         try:
             cmd = "/snap/bin/etcd.etcdctl member list"
             members = check_output(split(cmd), env={"ETCDCTL_API": "2"})
-            member_id = members.split(b':')[0].decode('utf-8')
+            member_id = members.split(b":")[0].decode("utf-8")
             break
         except CalledProcessError as ex:
             loop = loop + 1
@@ -216,7 +216,7 @@ def reconfigure_client_advertise():
                     "All member list tries failed during reconfiguring client advertise! Raising...",
                     "ERROR",
                 )
-                raise Exception('All member list tries failed') from ex
+                raise Exception("All member list tries failed") from ex
             time.sleep(1)
 
     raw_update = "/snap/bin/etcd.etcdctl member update {0} https://{1}:{2}"
@@ -225,10 +225,10 @@ def reconfigure_client_advertise():
 
 
 def shasum_file(filepath):
-    ''' Compute the SHA256sum of a file for verification purposes '''
+    """Compute the SHA256sum of a file for verification purposes"""
     BUF_SIZE = 65536  # 64kb chunk size
     shasum = hashlib.sha256()
-    with open(filepath, 'rb') as fp:
+    with open(filepath, "rb") as fp:
         while True:
             data = fp.read(BUF_SIZE)
             if not data:
@@ -242,22 +242,21 @@ def dismantle_cluster():
 
     This is a preparation step before restoring snapshot on the cluster.
     """
-    log('Disconnecting cluster members')
+    log("Disconnecting cluster members")
     etcdctl = EtcdCtl()
     etcd_conf = EtcdDatabag()
 
     my_name = etcd_conf.unit_name
-    endpoint = 'https://{}:{}'.format(etcd_conf.cluster_address,
-                                      etcd_conf.port)
+    endpoint = "https://{}:{}".format(etcd_conf.cluster_address, etcd_conf.port)
     for name, data in etcdctl.member_list(endpoint).items():
         if name != my_name:
-            log('Disconnecting {}'.format(name), hookenv.DEBUG)
+            log("Disconnecting {}".format(name), hookenv.DEBUG)
             loop = 0
             MAX_WAIT = 10
 
             while loop < MAX_WAIT:
                 try:
-                    etcdctl.unregister(data['unit_id'], endpoint)
+                    etcdctl.unregister(data["unit_id"], endpoint)
                     break
                 except EtcdCtl.CommandFailed as ex:
                     # Back-off timer to let cluster settle
@@ -265,31 +264,34 @@ def dismantle_cluster():
                     log("Disconnecting {} failed, retrying...".format(name), "WARNING")
                     if loop == MAX_WAIT:
                         log(
-                            "All tries for disconnecting the member {} failed! Raising...".format(name),
+                            "All tries for disconnecting the member {} failed! Raising...".format(
+                                name
+                            ),
                             "ERROR",
                         )
-                        raise Exception('Disconnecting a member from cluster failed') from ex
+                        raise Exception(
+                            "Disconnecting a member from cluster failed"
+                        ) from ex
                     time.sleep(1)
 
-    etcd_conf.cluster_state = 'new'
+    etcd_conf.cluster_state = "new"
     conf_path = os.path.join(etcd_conf.etcd_conf_dir, "etcd.conf.yml")
-    render('etcd3.conf', conf_path, etcd_conf.__dict__, owner='root',
-           group='root')
+    render("etcd3.conf", conf_path, etcd_conf.__dict__, owner="root", group="root")
 
 
 def rebuild_cluster():
     """Signal other etcd units to rejoin new cluster."""
-    log('Requesting peer members to rejoin cluster')
+    log("Requesting peer members to rejoin cluster")
     rejoin_request = uuid4().hex
     hookenv.leader_set(force_rejoin=rejoin_request)
 
 
-if __name__ == '__main__':
-    log('Performing etcd snapshot restore')
+if __name__ == "__main__":
+    log("Performing etcd snapshot restore")
     preflight_check()
     render_backup()
     dismantle_cluster()
-    service_stop(opts['etcd_daemon_process'])
+    service_stop(opts["etcd_daemon_process"])
     if is_v3_backup():
         log("v3 backup detected, restoring...", "INFO")
         restore_v3_backup()
@@ -300,6 +302,6 @@ if __name__ == '__main__':
         probe_forked_etcd()
         reconfigure_client_advertise()
         pkill_etcd(pid)
-    service_start(opts['etcd_daemon_process'])
+    service_start(opts["etcd_daemon_process"])
     rebuild_cluster()
     _run_atexit()
--- actions/snap-upgrade.py
+++ actions/snap-upgrade.py
@@ -31,39 +31,46 @@ from reactive.etcd import get_target_etcd_channel
 # Define some dict's containing paths of files we expect to see in
 # scenarios
 
-deb_paths = {'config': ['/etc/ssl/etcd/ca.crt',
-                        '/etc/ssl/etcd/server.crt',
-                        '/etc/ssl/etcd/server.key',
-                        '/etc/ssl/etcd/client.crt',
-                        '/etc/ssl/etcd/client.key',
-                        '/etc/default/etcd'],
-             'data': ['/var/lib/etcd/default']}
+deb_paths = {
+    "config": [
+        "/etc/ssl/etcd/ca.crt",
+        "/etc/ssl/etcd/server.crt",
+        "/etc/ssl/etcd/server.key",
+        "/etc/ssl/etcd/client.crt",
+        "/etc/ssl/etcd/client.key",
+        "/etc/default/etcd",
+    ],
+    "data": ["/var/lib/etcd/default"],
+}
 
 # Snappy only cares about the config objects. Data validation will come
 # at a later date. We can etcdctl ls / and then verify the data made it
 # post migration.
-snap_paths = {'config': ['/var/snap/etcd/common/etcd.conf.yml',
-                         '/var/snap/etcd/common/server.crt',
-                         '/var/snap/etcd/common/server.key',
-                         '/var/snap/etcd/common/ca.crt'],
-              'client': ['/var/snap/etcd/common/client.crt',
-                         '/var/snap/etcd/common/client.key'],
-              'common':  '/var/snap/etcd/common'}
+snap_paths = {
+    "config": [
+        "/var/snap/etcd/common/etcd.conf.yml",
+        "/var/snap/etcd/common/server.crt",
+        "/var/snap/etcd/common/server.key",
+        "/var/snap/etcd/common/ca.crt",
+    ],
+    "client": ["/var/snap/etcd/common/client.crt", "/var/snap/etcd/common/client.key"],
+    "common": "/var/snap/etcd/common",
+}
 
 
-def create_migration_backup(backup_package=''):
-    ''' Backup existing Etcd config/data paths if found and create a
-    tarball consisting of that discovered configuration '''
+def create_migration_backup(backup_package=""):
+    """Backup existing Etcd config/data paths if found and create a
+    tarball consisting of that discovered configuration"""
 
-    datestring = datetime.strftime(datetime.now(), '%Y%m%d_%H%M%S')
+    datestring = datetime.strftime(datetime.now(), "%Y%m%d_%H%M%S")
 
     if not backup_package:
-        pkg = '/home/ubuntu/etcd_migration_{}'
+        pkg = "/home/ubuntu/etcd_migration_{}"
         backup_package = pkg.format(datestring)
 
     if os.path.exists(backup_package):
-        msg = 'Backup package exists: {}'.format(backup_package)
-        action_set({'fail.message': msg})
+        msg = "Backup package exists: {}".format(backup_package)
+        action_set({"fail.message": msg})
         return False
 
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -72,94 +79,99 @@ def create_migration_backup(backup_package=''):
         os.makedirs(dated_path)
 
         # backup all the configuration data
-        for p in deb_paths['config']:
+        for p in deb_paths["config"]:
             if os.path.exists(p):
                 shutil.copy(p, dated_path)
             else:
-                log('Skipping copy for: {} - file not found'.format(p), 'WARN')
+                log("Skipping copy for: {} - file not found".format(p), "WARN")
 
         # backup the actual state of etcd's data
-        for p in deb_paths['data']:
+        for p in deb_paths["data"]:
             if os.path.exists(p):
-                cmd = 'rsync -avzp {} {}'.format(p, dated_path)
+                cmd = "rsync -avzp {} {}".format(p, dated_path)
                 check_call(split(cmd))
 
         try:
             # Create the tarball in its final location
-            shutil.make_archive(backup_package, 'gztar', tmpdir)
+            shutil.make_archive(backup_package, "gztar", tmpdir)
         except Exception as ex:
-            action_set({'fail.message': ex.message})
+            action_set({"fail.message": ex.message})
             return False
-    log('Created backup {}'.format(backup_package))
+    log("Created backup {}".format(backup_package))
     return True
 
 
 def install_snap(channel, classic=False):
-    ''' Handle installation of snaps, both from resources and from the snap
-    store. The only indicator we need is classic mode and the channel '''
-    snap.install('etcd', channel=channel, classic=classic)
+    """Handle installation of snaps, both from resources and from the snap
+    store. The only indicator we need is classic mode and the channel"""
+    snap.install("etcd", channel=channel, classic=classic)
 
 
 def deb_to_snap_migration():
     has_migrated = has_migrated_from_deb()
     if not has_migrated:
         try:
-            cmd = '/snap/bin/etcd.ingest'
+            cmd = "/snap/bin/etcd.ingest"
             check_call(split(cmd))
         except CalledProcessError as cpe:
-            log('Error encountered during ingest.', 'ERROR')
-            log('Error message: {}'.format(cpe.message))
-            action_fail('Migration failed')
+            log("Error encountered during ingest.", "ERROR")
+            log("Error message: {}".format(cpe.message))
+            action_fail("Migration failed")
 
-        for key_path in snap_paths['client']:
+        for key_path in snap_paths["client"]:
             chmod = "chmod 644 {}".format(key_path)
             call(split(chmod))
-        cmod = "chmod 755 {}".format(snap_paths['common'])
+        cmod = "chmod 755 {}".format(snap_paths["common"])
         call(split(cmod))
 
 
 def purge_deb_files():
-    probe_package_command = 'dpkg --list etcd'
+    probe_package_command = "dpkg --list etcd"
     return_code = call(split(probe_package_command))
     if return_code != 0:
         # The return code from dpkg --list when the package is
         # non existant
-        action_set({'dpkg.list.message': 'dpkg probe return_code > 0',
-                    'skip.package.purge': 'True'})
+        action_set(
+            {
+                "dpkg.list.message": "dpkg probe return_code > 0",
+                "skip.package.purge": "True",
+            }
+        )
         return
-    log('Purging deb configuration files post migration', 'INFO')
-    cmd = 'apt-get purge -y etcd'
+    log("Purging deb configuration files post migration", "INFO")
+    cmd = "apt-get purge -y etcd"
     try:
         check_call(split(cmd))
     except CalledProcessError as cpe:
-        action_fail({'apt.purge.message': cpe.message})
+        action_fail({"apt.purge.message": cpe.message})
 
-    for f in deb_paths['config']:
+    for f in deb_paths["config"]:
         try:
-            log('Removing file {}'.format(f), 'INFO')
+            log("Removing file {}".format(f), "INFO")
             os.remove(f)
         except FileNotFoundError:
-            k = 'purge.missing.{}'.format(os.path.basename(f))
-            msg = 'Did not purge {}. File not found.'.format(f)
+            k = "purge.missing.{}".format(os.path.basename(f))
+            msg = "Did not purge {}. File not found.".format(f)
             action_set({k: msg})
         except:
-            k = 'purge.error.{}'.format(f)
-            msg = 'Failed to purge {}. Manual removal required.'.format(k)
+            k = "purge.error.{}".format(f)
+            msg = "Failed to purge {}. Manual removal required.".format(k)
             action_set({k: msg})
 
 
 def has_migrated_from_deb():
-    for p in snap_paths['config']:
+    for p in snap_paths["config"]:
         # helpful when debugging
-        log("Scanning for file: {} {}".format(p, os.path.exists(p)), 'DEBUG')
+        log("Scanning for file: {} {}".format(p, os.path.exists(p)), "DEBUG")
         if not os.path.exists(p):
             return False
     return True
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Control flow of the action
-    backup_package = action_get('target')
-    backup = action_get('backup')
+    backup_package = action_get("target")
+    backup = action_get("backup")
 
     # We need to determine a default fallback channel since, there isn't an "auto" channel
     channel = get_target_etcd_channel()
@@ -169,16 +181,16 @@ if __name__ == '__main__':
     if backup:
         backup_status = create_migration_backup(backup_package)
         if not backup_status:
-            action_fail('Failed creating the backup. Refusing to proceed.')
+            action_fail("Failed creating the backup. Refusing to proceed.")
             sys.exit(0)
 
-    if not is_state('etcd.deb.migrated'):
-        install_snap('ingest/stable', True)
+    if not is_state("etcd.deb.migrated"):
+        install_snap("ingest/stable", True)
         deb_to_snap_migration()
     install_snap(channel, False)
     purge_deb_files()
-    remove_state('etcd.installed')
-    set_state('snap.installed.etcd')
-    remove_state('etcd.pillowmints')
+    remove_state("etcd.installed")
+    set_state("snap.installed.etcd")
+    remove_state("etcd.pillowmints")
     unitdata.kv().flush()
-    call(['hooks/config-changed'])
+    call(["hooks/config-changed"])
