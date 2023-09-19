c103c11cf73bb4c45bf7d4c56eee213ce36efa32
Adam Dyess <adam.dyess@canonical.com>
2023-09-19 13:24:37 -0500

    Upgrade tests to juju 3.1

--- tests/integration/test_etcd.py
+++ tests/integration/test_etcd.py
@@ -17,6 +17,14 @@ certs = [
 ]
 
 
+async def _unit_run(unit: Unit, jcmd: str, check: bool = True):
+    action = await unit.run(jcmd)
+    action = await action.wait()
+    if check:
+        assert action.status == "completed", f"Failed to run '{jcmd}'"
+    return action
+
+
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(series: str, ops_test: OpsTest):
     charm = await ops_test.build_charm(".")
@@ -29,14 +37,12 @@ async def test_build_and_deploy(series: str, ops_test: OpsTest):
     jcmd = "{} ETCDCTL_API=2 /snap/bin/etcd.etcdctl set juju rocks".format(
         " ".join(certs)
     )
-    action = await ops_test.model.applications["etcd"].units[0].run(jcmd)
-    assert action.status == "completed"
+    await _unit_run(ops_test.model.applications["etcd"].units[0], jcmd)
 
     nscmd = "{} ETCDCTL_API=2 /snap/bin/etcd.etcdctl set nested/data works".format(
         " ".join(certs)
     )
-    action = await ops_test.model.applications["etcd"].units[0].run(nscmd)
-    assert action.status == "completed"
+    await _unit_run(ops_test.model.applications["etcd"].units[0], nscmd)
 
 
 async def _get_leader(units: List[Unit]) -> Unit:
@@ -47,20 +53,19 @@ async def _get_leader(units: List[Unit]) -> Unit:
 
 async def test_leader_daemon_status(ops_test: OpsTest):
     leader = await _get_leader(ops_test.model.applications["etcd"].units)
-    action = await leader.run("systemctl is-active snap.etcd.etcd")
-    assert action.status == "completed"
-    assert "inactive" not in action.results["Stdout"]
-    assert "active" in action.results["Stdout"]
+    action = await _unit_run(leader, "systemctl is-active snap.etcd.etcd")
+    assert "inactive" not in action.results["stdout"]
+    assert "active" in action.results["stdout"]
 
 
 async def test_config_snapd_refresh(ops_test: OpsTest):
     leader = await _get_leader(ops_test.model.applications["etcd"].units)
-    action = await leader.run("snap get core refresh.timer")
-    assert len(action.results["Stdout"].strip()) == len("dayX")
+    action = await _unit_run(leader, "snap get core refresh.timer")
+    assert len(action.results["stdout"].strip()) == len("dayX")
 
     await ops_test.model.applications["etcd"].set_config({"snapd_refresh": "fri5"})
-    action = await leader.run("snap get core refresh.timer")
-    assert len(action.results["Stdout"].strip()) == len("fri5")
+    action = await _unit_run(leader, "snap get core refresh.timer")
+    assert len(action.results["stdout"].strip()) == len("fri5")
 
 
 async def test_node_scale_up(ops_test: OpsTest):
@@ -70,10 +75,9 @@ async def test_node_scale_up(ops_test: OpsTest):
 
     unit: Unit
     for unit in ops_test.model.applications["etcd"].units:
-        action = await unit.run("systemctl is-active snap.etcd.etcd")
-        assert action.status == "completed"
-        assert "inactive" not in action.results["Stdout"]
-        assert "active" in action.results["Stdout"]
+        action = await _unit_run(unit, "systemctl is-active snap.etcd.etcd")
+        assert "inactive" not in action.results["stdout"]
+        assert "active" in action.results["stdout"]
 
 
 async def test_cluster_health(ops_test: OpsTest):
@@ -81,23 +85,23 @@ async def test_cluster_health(ops_test: OpsTest):
         cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl cluster-health".format(
             " ".join(certs)
         )
-        action = await unit.run(cmd)
-        assert action.status == "completed"
-        assert "unhealthy" not in action.results["Stdout"]
-        assert "unavailable" not in action.results["Stdout"]
+        action = await _unit_run(unit, cmd)
+        assert "unhealthy" not in action.results["stdout"]
+        assert "unavailable" not in action.results["stdout"]
 
 
 async def test_leader_knows_all_members(ops_test: OpsTest):
     leader = await _get_leader(ops_test.model.applications["etcd"].units)
     cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl member list".format(" ".join(certs))
-    action = await leader.run(cmd)
-    assert action.status == "completed"
-    members = action.results["Stdout"].strip().split("\n")
+    action = await _unit_run(leader, cmd)
+    members = action.results["stdout"].strip().split("\n")
     assert "etcd cluster is unavailable" not in members
     assert len(members) == len(ops_test.model.applications["etcd"].units)
 
 
 async def test_node_scale_down(ops_test: OpsTest):
+    if len(ops_test.model.applications["etcd"].units) == 1:
+        return
     leader = await _get_leader(ops_test.model.applications["etcd"].units)
     await leader.destroy()
     await ops_test.model.wait_for_idle(wait_for_active=True, timeout=60 * 60)
@@ -106,7 +110,6 @@ async def test_node_scale_down(ops_test: OpsTest):
 async def test_health_check(ops_test: OpsTest):
     action = await ops_test.model.applications["etcd"].units[0].run_action("health")
     await action.wait()
-    assert action.status == "completed"
     assert "cluster is healthy" in action.results["output"]
 
 
@@ -115,7 +118,6 @@ async def test_snap_action(ops_test: OpsTest):
         await ops_test.model.applications["etcd"].units[0].run_action("snap-upgrade")
     )
     await action.wait()
-    assert action.status == "completed"
     await validate_running_snap_daemon(ops_test)
     await validate_etcd_fixture_data(ops_test)
 
@@ -129,23 +131,20 @@ async def test_snap_upgrade_to_three_oh(ops_test: OpsTest):
 
 async def validate_etcd_fixture_data(ops_test: OpsTest):
     jcmd = "{} ETCDCTL_API=2 /snap/bin/etcd.etcdctl get juju".format(" ".join(certs))
-    action = await ops_test.model.applications["etcd"].units[0].run(jcmd)
-    assert action.status == "completed"
-    assert "rocks" in action.results["Stdout"]
+    action = await _unit_run(ops_test.model.applications["etcd"].units[0], jcmd)
+    assert "rocks" in action.results["stdout"]
 
     nscmd = "{} ETCDCTL_API=2 /snap/bin/etcd.etcdctl get nested/data".format(
         " ".join(certs)
     )
-    action = await ops_test.model.applications["etcd"].units[0].run(nscmd)
-    assert action.status == "completed"
-    assert "works" in action.results["Stdout"]
+    action = await _unit_run(ops_test.model.applications["etcd"].units[0], nscmd)
+    assert "works" in action.results["stdout"]
 
 
 async def validate_running_snap_daemon(ops_test: OpsTest):
     cmd = "systemctl is-active snap.etcd.etcd"
-    action = await ops_test.model.applications["etcd"].units[0].run(cmd)
-    assert action.status == "completed"
-    assert "active" in action.results["Stdout"]
+    action = await _unit_run(ops_test.model.applications["etcd"].units[0], cmd)
+    assert "active" in action.results["stdout"]
 
 
 async def test_snapshot_restore(ops_test: OpsTest):
@@ -223,35 +222,35 @@ async def load_data(ops_test: OpsTest):
     cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl set /etcd2key etcd2value".format(
         " ".join(certs)
     )
-    await leader.run(cmd)
+    await _unit_run(leader, cmd)
     cmd = "{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 put etcd3key etcd3value".format(
         " ".join(certs[3:])
     )
-    await leader.run(cmd)
+    await _unit_run(leader, cmd)
 
 
 async def is_data_present(ops_test: OpsTest, version: str):
     leader = await _get_leader(ops_test.model.applications["etcd"].units)
     if version == "v2":
         cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl ls".format(" ".join(certs))
-        action = await leader.run(cmd)
+        action = await _unit_run(leader, cmd)
         log.info(action.status)
         log.info(action.results)
         return (
-            "etcd2key" in action.results["Stdout"]
-            if "Stdout" in action.results
+            "etcd2key" in action.results["stdout"]
+            if "stdout" in action.results
             else False
         )
     elif version == "v3":
         cmd = '{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 get "" --prefix --keys-only'.format(
             " ".join(certs[3:])
         )
-        action = await leader.run(cmd)
+        action = await _unit_run(leader, cmd)
         log.info(action.status)
         log.info(action.results)
         return (
-            "etcd3key" in action.results["Stdout"]
-            if "Stdout" in action.results
+            "etcd3key" in action.results["stdout"]
+            if "stdout" in action.results
             else False
         )
     return False
@@ -260,8 +259,9 @@ async def is_data_present(ops_test: OpsTest, version: str):
 async def delete_data(ops_test: OpsTest):
     leader = await _get_leader(ops_test.model.applications["etcd"].units)
     cmd = "{} ETCDCTL_API=2 /snap/bin/etcdctl rm /etcd2key".format(" ".join(certs))
-    await leader.run(cmd)
+    await _unit_run(leader, cmd)
+
     cmd = "{} ETCDCTL_API=3 /snap/bin/etcdctl --endpoints=http://localhost:4001 del etcd3key".format(
         " ".join(certs[3:])
     )
-    await leader.run(cmd)
+    await _unit_run(leader, cmd)
--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,4 +2,3 @@ import charms.unit_test
 
 charms.unit_test.patch_reactive()
 charms.unit_test.patch_module("charms.leadership")
-
