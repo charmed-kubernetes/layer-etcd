909c01f6e59fd9b64712ee5d1a88a4d19f5f0d12
Adam Dyess <adam.dyess@canonical.com>
2024-07-12 09:08:38 -0500

    Adding support from focal to noble

--- tests/validate-wheelhouse.sh
+++ tests/validate-wheelhouse.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 build_dir="$(mktemp -d)"
+charm="$(egrep '^name\S*:' ./metadata.yaml | awk '{ print $2 }')"
 function cleanup { rm -rf "$build_dir"; }
 trap cleanup EXIT
 
-charm build . --build-dir "$build_dir" --debug
-pip install -f "$build_dir/etcd/wheelhouse" --no-index --no-cache-dir "$build_dir"/etcd/wheelhouse/*
+charm-build . --build-dir "$build_dir" --debug
+pip install -f "$build_dir/$charm/wheelhouse" --no-index --no-cache-dir "$build_dir"/etcd/wheelhouse/*
